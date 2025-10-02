b5f4c95c7ca06b41873a91ca3a6f247a7b102500
bogdanradd <radbogdan044@gmail.com>
2025-10-02 23:32:13 +0300

    hw1

--- course/run_ansible_playbook_2.py
+++ course/run_ansible_playbook_2.py
@@ -1,7 +1,7 @@
 import ansible_runner
 
 out = ansible_runner.run(
-    private_data_dir='.',
+    private_data_dir='../project',
     playbook='r1_add_static_route.yaml',
     inventory='inv.ini',
     json_mode=True,
--- project/check_pylint.py
+++ project/check_pylint.py
@@ -1,3 +1,3 @@
 import pylint
-args = ['--rcfile=pylintrc_main' ,'../lib/connectors/swagger_conn.py']
+args = ['--rcfile=pylintrc_main' ,'../project']
 pylint.run_pylint(args)
